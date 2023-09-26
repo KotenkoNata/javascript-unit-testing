b98d83e2a4eabd8dfb510966aa51a50d52538b7c
KotenkoNata <kotenkonata8@gmail.com>
2023-09-26 15:24:52 +0200

    feat: write tetst for transformToNumber function

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,18 @@
+import {transformToNumber} from "./numbers";
+import {it, expect} from "vitest";
+
+it('should transform a string number to a number of type number', ()=>{
+    const input = '1';
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeTypeOf("number");
+});
+
+it('should yield NaN for non-transformable values', ()=>{
+    const input = 'invalid';
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeNaN();
+});
