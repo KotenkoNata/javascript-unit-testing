cc5c50a28fd9f2481dc9b7209f5d38c7a074fb3e
KotenkoNata <kotenkonata8@gmail.com>
2023-10-11 14:14:55 +0200

    feat: Testing DOM Functionalities

--- util/dom.test.js
+++ util/dom.test.js
@@ -1,4 +1,4 @@
-import {it, vi} from "vitest";
+import {expect, it, vi} from "vitest";
 import {showError} from "./dom";
 import * as fs from "fs";
 import path from 'path';
@@ -12,6 +12,12 @@ const document = window.document;
 document.write(htmlDocumentContent);
 
 vi.stubGlobal('document', document);
-it('first test', ()=>{
-    showError('test')
+it('should add an error paragraph to the id="errors" element', ()=>{
+    showError("Test");
+
+    const errorEl = document.getElementById('errors');
+
+    const errorParagraph = errorEl.firstElementChild;
+
+    expect(errorParagraph).not.toBeNull();
 })
